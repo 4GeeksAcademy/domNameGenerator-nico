526eedc14db2586fbc6f50bbf063bce7f4a3992b
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-05-20 22:10:08 +0000

    linea 10 modificada para que no se repitan los domains

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let domainNames = [];
 for (let i = 0; i < pronoun.length; i++) {
   for (let x = 0; x < adj.length; x++) {
     for (let y = 0; y < noun.length; y++) {
-      let domain = pronoun[i] + adj[x] + noun[i] + ".com";
+      let domain = pronoun[i] + adj[x] + noun[y] + ".com";
       domainNames.push(domain);
     }
   }
